a3be285f69dbe12c6dcc6345636b5bf550152da7
GONZO <youngyongpark@gmail.com>
2020-08-11 10:32:16 +0900

    Update dependencies (#1)
    * Update dependencies
    
    * Update dependencies

---
+++ test/migration/knex.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  client: 'mysql',
+  connection: {
+    database: 'dp_node',
+    host: '127.0.0.1',
+    port: 3306,
+    charset: 'utf8',
+    user: 'root',
+    password: null,
+  },
+};
