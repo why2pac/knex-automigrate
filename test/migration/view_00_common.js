088cea2660342633deb8d09565de98431d71022b
kyunghunkim <tirhande@gmail.com>
2025-01-27 22:59:15 +0900

    feature: add view example

--- test/migration/view_00_common.js
+++ test/migration/view_00_common.js
@@ -1,8 +1,26 @@
 /* eslint-disable newline-per-chained-call, global-require, no-undef */
 
 exports.auto = (migrator, knex) => [
-  migrator('VIEW_KEYVALS_ID', (view) => {
-    view.columns(['VAL']);
-    view.as(knex('KEYVALS_ID').select('VAL'));
+  migrator('KEYVALS_ID2', (view) => {
+    view.columns(['val', 'created_at', 'expiry_at']);
+    view.as(knex('KEYVALS_ID').select('VAL', 'CREATED_AT', 'EXPIRY_AT'));
+  }),
+  migrator('student_information', (view) => {
+    view.as(
+      knex('STUDENTS')
+        .select('STUDENT_ID', 'NAME', 'HOME_PHONE.PHONE as HOME_PHONE_NUMBER', 'MOBILE_PHONE.PHONE as MOBILE_PHONE_NUMBER')
+        .join(
+          'PHONES as HOME_PHONE',
+          'STUDENTS.HOME_PHONE_ID',
+          '=',
+          'HOME_PHONE.PHONE_ID',
+        )
+        .join(
+          'PHONES as MOBILE_PHONE',
+          'STUDENTS.MOBILE_PHONE_ID',
+          '=',
+          'MOBILE_PHONE.PHONE_ID',
+        ),
+    );
   }),
 ];
