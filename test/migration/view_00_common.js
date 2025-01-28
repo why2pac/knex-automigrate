98deab8e2fb504f38afe27e05fd4bfd087756a40
kyunghunkim <tirhande@gmail.com>
2025-01-28 17:16:35 +0900

    feature: add example and without safe mode

--- test/migration/view_00_common.js
+++ test/migration/view_00_common.js
@@ -2,25 +2,8 @@
 
 exports.auto = (migrator, knex) => [
   migrator('KEYVALS_ID2', (view) => {
-    view.columns(['val', 'created_at', 'expiry_at']);
+    // If view.columns() is missing,
+    // the columns will default to those defined in the 'select()' statement.
     view.as(knex('KEYVALS_ID').select('VAL', 'CREATED_AT', 'EXPIRY_AT'));
-  }),
-  migrator('student_information', (view) => {
-    view.as(
-      knex('STUDENTS')
-        .select('STUDENT_ID', 'NAME', 'HOME_PHONE.PHONE as HOME_PHONE_NUMBER', 'MOBILE_PHONE.PHONE as MOBILE_PHONE_NUMBER')
-        .join(
-          'PHONES as HOME_PHONE',
-          'STUDENTS.HOME_PHONE_ID',
-          '=',
-          'HOME_PHONE.PHONE_ID',
-        )
-        .join(
-          'PHONES as MOBILE_PHONE',
-          'STUDENTS.MOBILE_PHONE_ID',
-          '=',
-          'MOBILE_PHONE.PHONE_ID',
-        ),
-    );
   }),
 ];
