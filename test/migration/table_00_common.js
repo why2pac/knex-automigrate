a4d9074d453c2bef18840b7d4df04b7e72aa2119
Bread <sanghobread@gmail.com>
2025-04-07 14:51:54 +0900

    Add Test code

--- test/migration/table_00_common.js
+++ test/migration/table_00_common.js
@@ -9,6 +9,6 @@ exports.auto = (migrator, knex) => [
     table.datetime('EXPIRY_AT').nullable().comment('Expiry at, Timestamp.');
     table.datetime('CREATED_AT').notNullable().defaultTo(knex.fn.now()).comment('Created at, Timestamp.');
 
-    table.index(['VAL'], 'FT_IDX_KEYVALS_ID_VAL', { indexType: 'FULLTEXT' });
+    table.index(['VAL'], 'FT_IDX_KEYVALS_ID_VAL', { indexType: 'FULLTEXT', parser: 'ngram' });
   }),
 ];
